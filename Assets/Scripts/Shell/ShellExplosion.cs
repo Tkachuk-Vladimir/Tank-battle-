3a1e90ce83d68ab91059dce8dd39706b68b99c23
vovatk <tck.vova@ya.ru>
2021-09-23 16:39:27 +0300

    add file tank batlle

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;                        // Used to filter what the explosion affects, this should be set to "Players".
+    public ParticleSystem m_ExplosionParticles;         // Reference to the particles that will play on explosion.
+    public AudioSource m_ExplosionAudio;                // Reference to the audio that will play on explosion.
+    public float m_MaxDamage = 100f;                    // The amount of damage done if the explosion is centred on a tank.
+    public float m_ExplosionForce = 1000f;              // The amount of force added to a tank at the centre of the explosion.
+    public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
+    public float m_ExplosionRadius = 5f;                // The maximum distance away from the explosion tanks can be and are still affected.
+
+
+    private void Start()
+    {
+        // If it isn't destroyed by then, destroy the shell after it's lifetime.
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask); // создаётся массив калайдеров сферических
+        // в цикле ищем с каким сферическим калайдером столкнётся танк
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>(); // если сфера сталкнулась с танком то получаем его свойство тела
+
+            if (!targetRigidbody)// If they don't have a rigidbody, go on to the next collider.
+                continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);// если сфера сталкнулась с танком, толкаем танк
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();// если сфера сталкнулась с танком получаем доступ к коду Жизни
+
+            if (!targetHealth)
+                continue;
+            
+            float damage = CalculateDamage(targetRigidbody.position);// вычисляем урон, в зависимости от расстояния до снаряда   
+
+            targetHealth.TakeDamage(damage);// получаем доступ к коду Жизни, меняем уровень жизни
+        }
+
+        m_ExplosionParticles.transform.parent = null; 
+        m_ExplosionParticles.Play(); //включается эффект взрыва снаряда 
+        m_ExplosionAudio.Play(); // включается звук эффекта взрыва снаряда 
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration); // удаление эффекта взрыва
+        Destroy(gameObject); // удадаление снаряда
+    }
+
+    // функция вычисления урона. Чем ближе снаряд упал, тем больше урона и наоборот 
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position; // вычисляем растояние(вектор) до цели
+
+        float explosionDistance = explosionToTarget.magnitude;
+
+        // Calculate the proportion of the maximum distance (the explosionRadius) the target is away.
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        // Calculate damage as this proportion of the maximum possible damage.
+        float damage = relativeDistance * m_MaxDamage;
+
+        // Make sure that the minimum damage is always 0.
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+}
---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;   // это целочисленныя переменная, в которой хранитьсяномер игрока    
+    public Rigidbody m_Shell;        // переменная типа твёрдого тела, такая переменная бывает только в тут    
+    public Transform m_FireTransform;  // переенная типа точка 
+    public Slider m_AimSlider;         // переменная типа слайдр  
+    public AudioSource m_ShootingAudio;  
+    public AudioClip m_ChargingClip;     
+    public AudioClip m_FireClip;         
+    public float m_MinLaunchForce = 15f; 
+    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxChargeTime = 0.75f;
+
+    
+    private string m_FireButton;  // преременная типа стринг,туда помещаю слова 
+    private float m_CurrentLaunchForce;  
+    private float m_ChargeSpeed;         
+    private bool m_Fired;                
+
+    // функция что делать танку, когда он не стреляет 
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+    // утстановка клаиши выстрела и скорости перезарядки
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber; // установка клавиши выстрела - клавиша пробел
+
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;// скорость перезарядки
+    }
+    
+
+    private void Update()
+    {
+        m_AimSlider.value = m_MinLaunchForce;
+
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        else if (Input.GetButtonDown(m_FireButton))
+        {
+            m_Fired = false; // пререводим переменную в состояние false
+            m_CurrentLaunchForce = m_MinLaunchForce; // текущую силу устанвливаем в минимально значение 
+
+            m_ShootingAudio.clip = m_ChargingClip; // устанвливаем звук зарядки
+            m_ShootingAudio.Play(); // включаем звук зарядки 
+        }
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;// каждую секунду увеличиваем текущую силу
+            m_AimSlider.value = m_CurrentLaunchForce; // отрисовываем стрелку пропорционально текущей силе
+        }
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            Fire();
+        }
+    }
+
+
+    private void Fire()
+    {
+        m_Fired = true;
+
+        Rigidbody shellInstance = Instantiate(m_Shell,m_FireTransform.position, m_FireTransform.rotation) as Rigidbody; //создаюём снаряд в точке выстрела
+
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;// запускаем снаряд вперём с Текущей скоростью 
+
+        m_ShootingAudio.clip = m_FireClip;// устанавливаем звук выстрела
+        m_ShootingAudio.Play(); // включаем звук выстрела
+
+        m_CurrentLaunchForce = m_MinLaunchForce; // задаём минимальное значение текущей силе 
+         
+    }
+}
